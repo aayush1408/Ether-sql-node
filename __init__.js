a0833841cfa5fc65275ca6414d86ca385be32268
aayush1408 <aayushjaiswal0114@gmail.com>
2018-06-13 15:15:41 +0530

    intialise and init added

---
+++ __init__.js
@@ -0,0 +1,34 @@
+const sequelize = require('sequelize');
+const Web3 = require('web3');
+const settings = require('./settings.js');
+
+//setting up the connection
+const connection = new Sequelize(settings.database.db, settings.database.user, settings.database.password,{
+    host: 'localhost',
+    dialect:'postgres'
+  });
+ 
+function setupNodeSession(node_type, host='localhost', port=8545, api_token='')  {
+    let push_trace = 0;
+    if(node_type == 'Parity'){
+        node = new Web3(new Web3.providers.HttpProvider(`http://${host}:${port}`));
+    }
+    else if(node_type = 'Geth'){
+        node = new Web3(new Web3.providers.HttpProvider(`http://${host}:${port}`));
+    }
+    else if(node_type == 'Infura'){
+        // let network = host.split('.')[0]  
+        node = new Web3( new Web3.providers.HttpProvider(`https://${host}"/" ${api_token}`));
+    }
+    else{
+        console.log('Node not supported');
+    }
+    if(node.isConnected()){
+        console.log('Connected to node');
+    }
+    else{
+        console.log('Unable to connect')
+    }
+    return node,push_trace;
+}
+module.exports = setupNodeSession;
