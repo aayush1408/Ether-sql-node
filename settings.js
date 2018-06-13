af6ee924d354fc255ea476e04ee545a109d04e9e
aayush1408 <aayushjaiswal0114@gmail.com>
2018-06-13 15:10:23 +0530

    settings

---
+++ settings.js
@@ -0,0 +1,13 @@
+module.exports = {
+    database:{
+        user:'test',
+        password:'test',
+        'db':'test'
+    },
+    node:{
+        type:'Infura',
+        api_token:'7zUeGdm26WZRUzQVbZAD',
+        host:'kovan.infura.io',
+        port:'8545'
+    }
+}
