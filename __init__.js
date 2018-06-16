c6fd482f4cf2d6c2e6ac129777de1c6a446e33e7
aayush1408 <aayushjaiswal0114@gmail.com>
2018-06-16 16:38:20 +0530

    process part removed out of the funnc

--- __init__.js
+++ __init__.js
@@ -1,35 +1,58 @@
+//setting the limit of listeners
 require('events').EventEmitter.prototype._maxListeners = 0;
+
+
+//requiring the modules
 const Web3 = require('web3');
 const sequelize = require('sequelize');
 const settings = require('./settings');
+
+
+//getting the values from intialise file
 const setupNodeSession = require('./initialise').setupNodeSession;
 const connection = require('./initialise').connection;
+
+
+//Getting the models
 const Block = require('./models/blocks');
+
+//Acquiring queue
 const newQueue = require('./queue');
 const getBlockByNumber  = require('./listen');
 
+//getting the sql_block_number and creating jobs
 function newBlock(block_number){
     var job = newQueue.create('new_job',{
         block_number
-    }).unique(block_number);
-    job.save();
-    job.remove(function(error, job){
-        if( !error ) {
-             console.log(job.id);
-         }
-     })
-     console.log(job.data);
-     newQueue.process('new_job', function (job, done){
-        console.log('Processed');
-        getBlockByNumber(job.data.block_number);
-        done && done();
     });
+    job.on( 'complete', function () {
+    console.log("Job complete");
+    }).on( 'failed', function () {
+    console.log("Job failed");
+    });
+    job.save();
+    // job.remove(function(error, job){
+    //     if( !error ) {
+    //          console.log(job.id);
+    //      }
+    //  })
+     console.log(job.data.block_number);
     }
-   
+
+  //proceesing the jobs
+  newQueue.process('new_job',5, function (job, done){
+    console.log('Processed');
+    getBlockByNumber(job.data.block_number);
+    done && done();
+});    
+
+//getting the returned values from the setUpnodeSession function    
 const returnedValues = setupNodeSession(settings.node.type,settings.node.host,settings.node.port,settings.node.api_token);
 let node_session = returnedValues[0];
 let push_trace = returnedValues[1];
 
+
+//processBlock() runs every second and checks for the latest block number in node and  max sql node number
 function processBlock(){
     let node_block_number = node_session.eth.blockNumber;
     connection.query(`SELECT MAX(block_number) FROM blocks`).then((result)=>{
@@ -46,4 +69,5 @@ function processBlock(){
     });        
 }
 
+//calling the processBlock every second
 setInterval(processBlock,1000);
