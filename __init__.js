8b720dd17b6979216cb895e51b638c2052d0b8db
aayush1408 <aayushjaiswal0114@gmail.com>
2018-06-16 18:49:05 +0530

    converted to integer

--- __init__.js
+++ __init__.js
@@ -39,12 +39,7 @@ function newBlock(block_number){
      console.log(job.data.block_number);
     }
 
-  //proceesing the jobs
-  newQueue.process('new_job',5, function (job, done){
-    console.log('Processed');
-    getBlockByNumber(job.data.block_number);
-    done && done();
-});    
+ 
 
 //getting the returned values from the setUpnodeSession function    
 const returnedValues = setupNodeSession(settings.node.type,settings.node.host,settings.node.port,settings.node.api_token);
@@ -55,6 +50,8 @@ let push_trace = returnedValues[1];
 //processBlock() runs every second and checks for the latest block number in node and  max sql node number
 function processBlock(){
     let node_block_number = node_session.eth.blockNumber;
+    console.log('Aaaaaa');
+    console.log(node_block_number);
     connection.query(`SELECT MAX(block_number) FROM blocks`).then((result)=>{
         var sql_block_number = result[0][0].max;
         console.log(sql_block_number);
@@ -63,11 +60,18 @@ function processBlock(){
         }
         else{
             for(let i=0;i<5;i++){
-                newBlock(sql_block_number+i);
+                newBlock(parseInt(sql_block_number)+i);
             }
         }
     });        
 }
-
+ 
 //calling the processBlock every second
 setInterval(processBlock,1000);
+
+// //proceesing the jobs
+newQueue.process('new_job',5, function (job, done){
+    console.log('Processed');
+    getBlockByNumber(job.data.block_number);
+    done && done();
+});    
